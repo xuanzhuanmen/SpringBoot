65ebca58ddb05dae45057010c183c27a4913b5df
chengjaijun <825997676@qq.com>
2018-11-17 18:36:22 +0800

    新增UserController

---
+++ src/main/java/com/cheng/springboothalo/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.cheng.springboothalo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class UserController {
+
+    @ResponseBody
+    @RequestMapping("/getUserName")
+    public String getUserName() {
+        return "Halo Stranger!";
+    }
+}
